7af1b8fb229248f921efb0bf853b68720d782aea
Ankit Pandey <imakki1410@gmail.com>
2024-07-07 01:29:12 +0530

    solved cors error

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  // origin: 'http://localhost:3000',
+  origin: 'https://watchwave-sooty.vercel.app',
   credentials: true
 }
 app.use(cors(corsOptions));
